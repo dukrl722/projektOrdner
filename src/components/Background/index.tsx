013cc2fee386550c932a7427422cfcda57776280
ViniCL <vinilodi2013@gmail.com>
2022-04-04 18:22:02 -0300

    C1 - Desenvolvimento

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+import { styles } from "./styles";
+import { theme } from '../../global/styles/theme';
+
+interface Props {
+    children: ReactNode
+}
+
+export function Background({ children } : Props) {
+
+    const { primary, secondary20 } = theme.colors;
+
+    return (
+        <LinearGradient style={styles.container} colors={[primary, secondary20]} >
+            {children}
+        </LinearGradient>
+    )
+}
