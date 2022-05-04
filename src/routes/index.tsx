fdda980cc4f33c0e6d6bd404db0957e4bfb3b216
Gabriel Refosco <gbrefosco@gmail.com>
2022-05-04 17:35:04 -0300

    aprimora tabBar e navegação geral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { theme } from '../global/styles/theme';
 
@@ -12,65 +13,75 @@ import { Home } from "../screens/Home";
 
 import { Feather } from '@expo/vector-icons'
 
-const { Navigator, Screen } = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export function AuthRoutes() {
-    return (
-        <NavigationContainer>
-            <Navigator
 
+    const stackSettings = {
+        headerShown: false,
+        headerStyle: {
+            backgroundColor: theme.colors.secondary100
+        },
+        tabBarActiveTintColor: '#fff',
+        tabBarStyle: theme.tabBar
+    }
+
+    const tabSettings = {
+        headerShown: false
+    }
+
+    const tabBarTheme = theme.tabBar;
+
+    const TabNavigator = () => {
+        return (
+            <Tab.Navigator
                 screenOptions={{
                     headerShown: false,
-                    headerStyle: {
-                        backgroundColor: theme.colors.secondary100
+                    tabBarStyle: {
+                        backgroundColor: '#DADADA',
+                        height: 80,
+                        paddingBottom: 15,
+                        borderRadius: 15
                     },
-                    tabBarActiveTintColor: '#fff',
-                    tabBarStyle: theme.tabBar
-                }}>
-
-                <Screen
-                    name="CHAMA O MODAL DE AJUDA/TUTORIAL"
-                    component={SignUp}
+                    tabBarItemStyle: {
+                        marginHorizontal: 20
+                    },
+                    tabBarShowLabel: false
+                }}
+            >
+                <Tab.Screen
+                    name='HomePage'
+                    component={Home}
                     options={{
-                        tabBarIcon: () => {
-                            return <Feather name="help-circle" size={30} color="#000" />
-                        },
-                        tabBarLabel: () => { return null },
+                        tabBarIcon: ({ focused, color }) => <Feather name='home' size={30} color={focused ? '#2c9cf7' : 'gray'} />,
                     }}
                 />
-                <Screen
-                    name="DIFERECIONA PARA A TELA INICIAL"
+                <Tab.Screen
+                    name='Select'
                     component={Select}
                     options={{
-                        tabBarIcon: () => {
-                            return <Feather name="home" size={30} color="#000" />
-                        },
-                        tabBarLabel: () => { return null },
+                        tabBarIcon: ({ focused }) => <Feather name='user' size={30} color={focused ? '#2c9cf7' : 'gray'} />,
                     }}
                 />
-                <Screen
-                    name="DIFERECIONA PARA O PERFIL"
-                    component={Login}
-                    options={{
-                        tabBarIcon: () => {
-                            return <Feather name="user" size={30} color="#000" />
-                        },
-                        tabBarLabel: () => { return null },
-                    }}
-                />
-                <Screen
-                    name="CHAMA O MODAL DE LOG OUT"
-                    component={SignUp}
-                    options={{
-                        tabBarIcon: () => {
-                            return <Feather name="log-out" size={30} color="#000" />
-                        },
-                        tabBarLabel: () => { return null },
-                    }}
-                />
-       
-            </Navigator>
-        </NavigationContainer>
+            </Tab.Navigator>
+        )
+    }
 
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                    <Stack.Screen 
+                        name='Home'
+                        options={tabSettings}
+                        component={TabNavigator}
+                    />
+                    <Stack.Screen
+                        name='Login'
+                        component={Login}
+                        options={stackSettings}
+                    />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
