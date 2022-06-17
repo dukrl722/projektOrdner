81b76e6df433df23f122ae684a2816b9c9f5e9b0
Lucas L de Souza <lucas@lucaslenoch.com.br>
2022-06-17 01:25:25 -0300

    Style modificado com ajuda de ranchocrudes

--- src/components/AreaInteresse/styles.ts
+++ src/components/AreaInteresse/styles.ts
@@ -3,26 +3,22 @@ import { theme } from "../../global/styles/theme";
 
 export const themes = StyleSheet.create({
     container: {
-        flex:1,
         flexDirection:'row',
         justifyContent: "center",
         borderWidth:2,
         borderColor:'purple',
-        width:360,
         height:70,
+        marginHorizontal:10
     },
     textView:{
-        width:130,
-        right:110,
+         flex:1,
         fontSize:12,
+        color:'white'
     },
     editar: {
+        flex:1,
         backgroundColor:'#0066ff',
-        width:105,
         height:60,
-        top:3,
-        left:135,
-        position:'absolute',
         alignItems:'center',
         color: theme.colors.menuInputBorder,
         fontFamily: theme.fonts.text400,
@@ -30,13 +26,10 @@ export const themes = StyleSheet.create({
         fontSize: 16,
     },
     excluir:{
+        flex:1,
         backgroundColor:'#ff6611',
-        position:'absolute',
         alignItems:'center',
-        width:105,
         height:60,
-        left:248,
-        top:3,
         color: theme.colors.menuInputBorder,
         fontFamily: theme.fonts.text400,
         borderWidth:2,
